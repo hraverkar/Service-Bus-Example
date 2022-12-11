d73edd64dcb217ca722d0e5e109cc93092da2c17
hraverkar <hraverkar@hraverkarvms>
2022-12-11 07:06:40 +0000

    Add project files.

---
+++ AzureServiceBusDemo.Core/Class1.cs
@@ -0,0 +1,111 @@
+﻿using Azure.Messaging.ServiceBus;
+using AzureServiceBusDemo.Core;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Concurrent;
+using System.Text;
+using System.Text.Json;
+
+namespace AzureServiceBusDemo.Core
+{
+    public interface IMessageBus
+    {
+        Task PublishMessageAsync<T>(T message);
+    }
+
+    public interface IMessageBusFactory
+    {
+
+        IMessageBus GetClient(string connectionString, string sender);
+    }
+    public class AzureServiceBus : IMessageBus
+    {
+        private readonly ServiceBusSender _serviceBusSender;
+
+        public AzureServiceBus(ServiceBusSender serviceBusSender)
+        {
+            _serviceBusSender = serviceBusSender;
+        }
+        public async Task PublishMessageAsync<T>(T message)
+        {
+            var jsonString = JsonSerializer.Serialize(message);
+            var serviceBusMessage = new ServiceBusMessage(Encoding.UTF8.GetBytes(jsonString));
+            await _serviceBusSender.SendMessageAsync(serviceBusMessage);
+        }
+
+        public static IMessageBus Create(ServiceBusSender sender)
+        {
+            return new AzureServiceBus(sender);
+        }
+    }
+
+    public class AzureServiceBusFactory : IMessageBusFactory
+    {
+        private readonly object _lockObject = new object();
+        private readonly ConcurrentDictionary<string, ServiceBusClient> _client =
+            new ConcurrentDictionary<string, ServiceBusClient>();
+        private readonly ConcurrentDictionary<string, ServiceBusSender> _sender =
+            new ConcurrentDictionary<string, ServiceBusSender>();
+
+        public IMessageBus GetClient(string connectionString, string senderName)
+        {
+            var key = $"{connectionString}-{senderName}";
+            if (this._sender.ContainsKey(key) && !this._sender[key].IsClosed)
+            {
+                return AzureServiceBus.Create(this._sender[key]);
+            }
+
+            var client = this.GetServiceBusClient(connectionString);
+
+            lock (this._lockObject)
+            {
+                if (this._sender.ContainsKey(key) && this._sender[key].IsClosed)
+                {
+                    if (this._sender[key].IsClosed)
+                    {
+                        this._sender[key].DisposeAsync().GetAwaiter().GetResult();
+                    }
+                    return AzureServiceBus.Create(this._sender[key]);
+                }
+
+                var sender = client.CreateSender(senderName);
+                this._sender[key] = sender;
+            }
+            return AzureServiceBus.Create(this._sender[key]);
+
+        }
+
+        protected virtual ServiceBusClient GetServiceBusClient(string connectionString)
+        {
+            var key = $"{connectionString}";
+            lock(this._lockObject)
+            {
+                if (this.ClientDoesnotExistOrIsClosed(connectionString))
+                {
+                    var client = new ServiceBusClient(connectionString, new ServiceBusClientOptions
+                    {
+                        TransportType = ServiceBusTransportType.AmqpTcp
+                    });
+                    this._client[key] = client;
+
+                }
+                return this._client[key];
+            }
+        }
+
+        private bool ClientDoesnotExistOrIsClosed(string connectionString)
+        {
+            return !this._client.ContainsKey(connectionString) 
+                || this._client[connectionString].IsClosed;
+        }
+
+    }
+
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection AddAzureServiceBusFactory (this IServiceCollection services)
+        {
+            services.AddSingleton<IMessageBusFactory, AzureServiceBusFactory>();
+            return services;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using Azure.Messaging.ServiceBus;
+using AzureServiceBusDemo.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+        public async static Task Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            services.AddTransient<App>();
+            services.AddAzureServiceBusFactory();
+            var serviceProvider = services.BuildServiceProvider();
+            await serviceProvider.GetService<App>().Run();
+
+        }
+    }
+
+    public class App
+    {
+        private readonly IMessageBusFactory _messageBusFactory;
+        public App(IMessageBusFactory messageBusFactory)
+        {
+            _messageBusFactory = messageBusFactory;
+        }
+
+        public async Task Run()
+        {
+            string connectionString = "Endpoint=sb://hraverkarmyservicebus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=pMSMW/2urwq9t7DEm9F9xzYwOIw5xI6yn9ajc+LjQZM=";
+            string queueName = "myqueue";
+            var client = this._messageBusFactory.GetClient(connectionString, queueName);
+            await client.PublishMessageAsync(new
+            {
+                FirstName = "Bob",
+                LastName = "Smith"
+            });
+        }
+    }
+
+}
